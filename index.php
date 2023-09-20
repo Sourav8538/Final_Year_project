e3de42f690e3dac6e35ab9b115a74ed060bc9a39
Sσυɾαʋ Gԋσʂԋ <113099442+Sourav8538@users.noreply.github.com>
2023-09-20 23:09:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?PHP
-    // header('Location: http://localhost/tutorials/authentication/login.php');
+     header('Location: http://localhost/tutorials/authentication/login.php');
     include('Header.php');
     include('Footer.php')
 ?>
